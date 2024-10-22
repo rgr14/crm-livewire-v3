d08701b4b7d060f56f662c5e991743b98258cdb6
rgr14 <rogeraugustosoares@gmail.com>
2024-10-22 20:46:18 -0300

    validation rules

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -4,14 +4,21 @@ namespace App\Livewire\Auth;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Register extends Component
 {
-    public ?string $name;
-    public ?string $email;
-    public ?string $email_confirmation;
-    public ?string $password;
+    #[Rule(['required', 'max:255'])]
+    public ?string $name = null;
+
+    #[Rule(['required', 'email', 'max:255', 'confirmed'])]
+    public ?string $email = null;
+
+    public ?string $email_confirmation = null;
+
+    #[Rule(['required'])]
+    public ?string $password = null;
 
     public function render()
     {
@@ -20,6 +27,8 @@ class Register extends Component
 
     public function submit(): void
     {
+        $this->validate();
+
         User::query()->create([
             'name' => $this->name,
             'email' => $this->email,
