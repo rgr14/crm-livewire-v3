d4312e3cdf8eef9675aa19d87b9b902f22f5205d
rgr14 <rogeraugustosoares@gmail.com>
2024-10-31 22:55:06 -0300

    user model hass all the methods to crontrol permission for the giver user

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -44,5 +45,22 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function permissions(): belongsToMany
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+
+    public function givePermissionTo(string $key): void
+    {
+        $this->permissions()->firstOrCreate([
+            'key' => $key,
+        ]);
+    }
+
+    public function hasPermissionTo(string $key): bool
+    {
+        return $this->permissions()->where(compact('key'))->exists();
     }
 }
---
+++ tests/Feature/PermissionControlTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\Permission;
+use App\Models\User;
+
+it('should be able to give an user a permission to do something', function () {
+    /** @var User $user */
+    $user = User::factory()->create();
+
+    $user->givePermissionTo('be an admin');
+
+    expect($user)
+        ->hasPermissionTo('be an admin')
+        ->toBeTrue();
+
+    \Pest\Laravel\assertDatabaseHas('permissions', [
+        'key' => 'be an admin',
+    ]);
+
+    \Pest\Laravel\assertDatabaseHas('permission_user', [
+        'user_id' => $user->id,
+        'permission_id' => Permission::where('key', '=', 'be an admin')->first()->id,
+    ]);
+});
