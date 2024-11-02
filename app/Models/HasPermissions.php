717d2a5877c566c7d089bf27f8e75a574c71edae
rgr14 <rogeraugustosoares@gmail.com>
2024-11-02 18:02:51 -0300

    refactoring permissions to his a trait

---
+++ app/Models/HasPermissions.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Facades\Cache;
+
+trait HasPermissions
+{
+    public function permissions(): belongsToMany
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+
+    public function givePermissionTo(string $key): void
+    {
+        $this->permissions()->firstOrCreate([
+            'key' => $key,
+        ]);
+
+        Cache::forget($this->getPermissionCacheKey());
+        Cache::rememberForever(
+            $this->getPermissionCacheKey(),
+            fn() => $this->permissions
+        );
+    }
+
+    public function hasPermissionTo(string $key): bool
+    {
+        /** @var Collection $permissions */
+        $permissions = Cache::get($this->getPermissionCacheKey(), $this->permissions);
+
+        return $permissions
+            ->where("key", '=', $key)
+            ->isNotEmpty();
+    }
+
+    private function getPermissionCacheKey(): string
+    {
+        return "user::{$this->id}::permissions";
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,17 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Cache;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasPermissions;
 
     /**
      * The attributes that are mass assignable.
@@ -48,38 +45,4 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-
-    public function permissions(): belongsToMany
-    {
-        return $this->belongsToMany(Permission::class);
-    }
-
-    public function givePermissionTo(string $key): void
-    {
-        $this->permissions()->firstOrCreate([
-            'key' => $key,
-        ]);
-
-        Cache::forget($this->getPermissionCacheKey());
-        Cache::rememberForever(
-            $this->getPermissionCacheKey(),
-            fn() => $this->permissions
-        );
-    }
-
-    public function hasPermissionTo(string $key): bool
-    {
-        /** @var Collection $permissions */
-        $permissions = Cache::get($this->getPermissionCacheKey(), $this->permissions);
-
-        return $permissions
-            ->where("key", '=', $key)
-            ->isNotEmpty();
-    }
-
-    private function getPermissionCacheKey(): string
-    {
-        return "user::{$this->id}::permissions";
-    }
-
 }
